5b8ff3c2a9ad1fb79c630c3141773224dd4be5af
tcotidiane33 <tcotidiane33@gmail.com>
2023-11-10 12:06:53 +0100

    uplaod data add pages -sign up -sign in - dashboards - produits -

--- dist/script.js
+++ dist/script.js
@@ -1,30 +1,69 @@
-// Récupérez une référence à la base de données Firebase
-var database = firebase.database().ref('https://novohub-default-rtdb.firebaseio.com/');
+// Route pour la page de sign up
+app.get('/signup', (req, res) => {
+  res.render('signup'); // Crée un fichier signup.ejs pour la page de sign up
+});
 
-// Sélectionnez l'élément HTML où vous souhaitez afficher les données
-var userList = document.getElementById('userList');
+// Route pour la page de sign in
+app.get('/signin', (req, res) => {
+  res.render('signin'); // Crée un fichier signin.ejs pour la page de sign in
+});
 
-// Écoutez les modifications de données
-database.on('value', function(snapshot) {
-  userList.innerHTML = ''; // Effacez le contenu précédent
+// Route pour traiter la soumission du formulaire de sign up
+app.post('/signup', async (req, res) => {
+  const { email, password } = req.body;
 
-  snapshot.forEach(function(childSnapshot) {
-    var user = childSnapshot.val();
-    var userId = childSnapshot.key;
+  // Insère les données dans la collection MongoDB
+  const novoDb = client.db('novo');
+  const collection = novoDb.collection('users');
+  await collection.insertOne({ email, password });
+  
+  res.send('Inscription réussie !');
+});
 
-    // Créez des éléments HTML pour afficher les informations de l'utilisateur
-    var userDiv = document.createElement('div');
-    userDiv.className = 'card';
-    userDiv.innerHTML = '<div class="card-body">' +
-      '<h5 class="card-title">ID de l\'utilisateur : ' + userId + '</h5>' +
-      '<p class="card-text">Email : ' + user.email + '</p>' +
-      '<p class="card-text">Nom d\'utilisateur : ' + user.username + '</p>' +
-      '<p class="card-text">Mot de passe : ' + user.password + '</p>' +
-      '</div>';
+// Route pour traiter la soumission du formulaire de sign in
+app.post('/signin', async (req, res) => {
+  const { email, password } = req.body;
 
-    // Ajoutez l'élément HTML à la liste des utilisateurs
-    userList.appendChild(userDiv);
-  });
-}, function(error) {
-  console.error('Erreur lors de la récupération des données :', error);
+  // Vérifie les informations d'identification dans la collection MongoDB
+  const novoDb = client.db('novo');
+  const collection = novoDb.collection('users');
+  const user = await collection.findOne({ email, password });
+
+  if (user) {
+      res.send('Connexion réussie !');
+  } else {
+      res.send('Échec de la connexion. Vérifiez vos informations d\'identification.');
+  }
+});
+
+// Route pour le tableau de bord
+app.get('/dashboard', async (req, res) => {
+  try {
+      const novoDb = client.db('novo');
+      const collections = await novoDb.listCollections().toArray();
+
+      res.render('dashboard', { collections });
+  } catch (err) {
+      console.error('Failed to retrieve collection names:', err);
+      res.send('Failed to retrieve collection names. Please try again later.');
+  }
+});
+
+
+// Route pour traiter la mise à jour d'une collection
+app.post('/dashboard/update', async (req, res) => {
+  const collectionName = req.body.collection;
+  
+  // Ajoute ici la logique de mise à jour selon tes besoins
+
+  res.send(`Mise à jour réussie pour la collection ${collectionName}`);
+});
+
+// Route pour traiter la suppression d'une collection
+app.post('/dashboard/delete', async (req, res) => {
+  const collectionName = req.body.collection;
+
+  // Ajoute ici la logique de suppression selon tes besoins
+
+  res.send(`Suppression réussie pour la collection ${collectionName}`);
 });
